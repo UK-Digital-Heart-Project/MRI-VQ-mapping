95dec8fa09b70f707edf8952945a56d88532da52
ptokarcz <37544040+ptokarcz@users.noreply.github.com>
2020-04-22 06:34:52 +0100

    Add files via upload

---
+++ Perfusion-GUIDE-Project-Hybrid/pft_GetMappingThreshold.m
@@ -0,0 +1,58 @@
+function PC = pft_GetMappingThreshold(StartPC)
+
+Options.Resize      = 'off';
+Options.WindowStyle = 'modal';
+Options.Interpreter = 'tex';
+
+Prompt = { 'Mapping threshold - per cent of maximum AIF' };
+
+Starts = { sprintf('%.2f', StartPC) };
+
+Layout = zeros(1, 2, 'int16');
+Layout(:, 1) = 1;
+Layout(:, 2) = 45;
+
+Answers = inputdlg(Prompt, 'Mapping parameters', Layout, Starts, Options);
+
+Amended = false;
+
+if (length(Answers) == length(Starts))
+  PC = str2double(Answers{1});
+    
+  if ~isnumeric(PC) 
+    PC = int32(str2double(Starts{1}));
+    Amended = true;
+  elseif isnan(PC) || isinf(PC) || ~isreal(PC)
+    PC = int32(str2double(Starts{1}));
+    Amended = true;
+  end 
+else
+  PC = str2double(Starts{1}); 
+  Amended = true;
+end
+
+if (PC < 1.0)
+  PC = 1.0;
+  Amended = true;
+elseif (PC > 50.0)
+  PC = 50.0;
+  Amended = true;
+end
+
+if (Amended == true)
+  beep;  
+    
+  Warning = { 'Input amended:', ...
+              ' ', ...
+              sprintf('Masking threshold = %.2f %%', PC), ...
+              ' ' };
+               
+  Title   =   'Error correction';
+  
+  h = warndlg(Warning, Title, 'modal');                
+  uiwait(h);
+  delete(h);
+end
+  
+end
+

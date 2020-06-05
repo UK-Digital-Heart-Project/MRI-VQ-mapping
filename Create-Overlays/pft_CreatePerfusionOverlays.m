39ffbb39a8ff59fdeed1dfc34a8fe77605e0b3e1
ptokarcz <37544040+ptokarcz@users.noreply.github.com>
2020-06-05 18:33:39 +0100

    Colormaps added
    Low-is-blue versions of "hot" and "Red Hot"

--- Create-Overlays/pft_CreatePerfusionOverlays.m
+++ Create-Overlays/pft_CreatePerfusionOverlays.m
@@ -631,6 +631,9 @@ M = find(strcmpi(handles.MatlabLutNames, handles.CurrentColormapName), 1, 'first
 if ~isempty(M)
   if strcmpi(handles.CurrentColormapName, 'vga')
     handles.Colormap = vga; 
+  elseif strcmpi(handles.CurrentColormapName, 'hot-low-blue')
+    handles.Colormap = eval(sprintf('%s(%s)', 'hot', handles.CurrentColormapSize));
+    handles.Colormap(1, :) = [ 0.00 0.25 1.00 ];      
   else
     handles.Colormap = eval(sprintf('%s(%s)', handles.CurrentColormapName, handles.CurrentColormapSize));
   end
@@ -671,7 +674,10 @@ M = find(strcmpi(handles.MatlabLutNames, handles.CurrentColormapName), 1, 'first
 
 if ~isempty(M)
   if strcmpi(handles.CurrentColormapName, 'vga')
-    handles.Colormap = vga; 
+    handles.Colormap = vga;     
+  elseif strcmpi(handles.CurrentColormapName, 'hot-low-blue')
+    handles.Colormap = eval(sprintf('%s(%s)', 'hot', handles.CurrentColormapSize));
+    handles.Colormap(1, :) = [ 0.00 0.25 1.00 ];  
   else
     handles.Colormap = eval(sprintf('%s(%s)', handles.CurrentColormapName, handles.CurrentColormapSize));
   end
